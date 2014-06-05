e14061dcadd1695e273a4b851262c2bb0d97f49e
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-05 10:34:12 +0100

    Add API for suspending/unsuspending.

---
+++ lib/peapod/library.rb
@@ -0,0 +1,36 @@
+require 'peapod/podcast.rb'
+
+module Peapod
+  class Library
+    def initialize(source)
+      @source = source
+    end
+
+    def on_podcast_with_id(pod_id)
+      PodcastProxy.new(source, pod_id)
+    end
+  end
+
+  class PodcastProxy
+    def initialise(source, pod_id)
+      @source = source
+      @pod_id = pod_id
+    end
+
+    def set_suspended!(is_suspended)
+      @source.set_suspended(pod_id, is_suspended)
+    end
+
+    def suspend!
+      set_suspended(true)
+    end
+
+    def unsuspend!
+      set_suspended(false)
+    end
+
+    def suspended?
+      @source.suspended?(pod_id)
+    end
+  end
+end
