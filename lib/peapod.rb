314e51c40deb432410673beab76f1e99c3df7fa6
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-05 10:41:42 +0100

    Add self.library method.

--- lib/peapod.rb
+++ lib/peapod.rb
@@ -1,5 +1,10 @@
-require "peapod/version"
+require 'peapod/library'
+require 'peapod/library_database'
+require 'peapod/version'
 
 module Peapod
-  # Your code goes here...
+  def self.library
+    ld = LibraryDatabase.from_dbpasswd
+    Library.new(ld)
+  end
 end
