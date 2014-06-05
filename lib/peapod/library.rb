0a622ae7f37362908de979850b3c0153e214c2f2
mattbw <matt.windsor@ury.york.ac.uk>
2014-06-05 10:51:12 +0100

    Typos galore

--- lib/peapod/library.rb
+++ lib/peapod/library.rb
@@ -1,5 +1,3 @@
-require 'peapod/podcast.rb'
-
 module Peapod
   class Library
     def initialize(source)
@@ -7,30 +5,30 @@ module Peapod
     end
 
     def on_podcast_with_id(pod_id)
-      PodcastProxy.new(source, pod_id)
+      PodcastProxy.new(@source, pod_id)
     end
   end
 
   class PodcastProxy
-    def initialise(source, pod_id)
+    def initialize(source, pod_id)
       @source = source
       @pod_id = pod_id
     end
 
     def set_suspended!(is_suspended)
-      @source.set_suspended(pod_id, is_suspended)
+      @source.set_suspended!(@pod_id, is_suspended)
     end
 
     def suspend!
-      set_suspended(true)
+      set_suspended!(true)
     end
 
     def unsuspend!
-      set_suspended(false)
+      set_suspended!(false)
     end
 
     def suspended?
-      @source.suspended?(pod_id)
+      @source.suspended?(@pod_id)
     end
   end
 end
